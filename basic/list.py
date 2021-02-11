3414542124a3b9f5339d144d82d458332d3aa230
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-02-11 23:43:49 +0900

    something list method

---
+++ basic/list.py
@@ -0,0 +1,22 @@
+# Initialize
+list_a = [1,2,3,4,5,6,7,8,9,10]
+
+# slice
+list_b = list_a[1:10:3]
+print(list_b)
+
+# append
+list_a.append('apple')
+print(list_a)
+
+# extend
+list_a.extend(['banana', 'melon'])
+print(list_a)
+
+# Insert
+list_a.insert(2, 'grape')
+print(list_a)
+
+# clear
+list_a.clear()
+print(list_a)
