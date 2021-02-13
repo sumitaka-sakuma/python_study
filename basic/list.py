52df3c9dcba73700a8f5bd2b24ac47e3c546a368
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-02-13 10:36:44 +0900

    practice pythons list

--- basic/list.py
+++ basic/list.py
@@ -18,5 +18,33 @@ list_a.insert(2, 'grape')
 print(list_a)
 
 # clear
-list_a.clear()
+#list_a.clear()
+#print(list_a)
+
+# remove
+list_a.remove(10)
+print(list_a)
+
+# pop
+value = list_a.pop()
+print(list_a, value)
+
+# count
+print(list_a.count(1))
+
+# index 
+print(list_a.index(7))
+
+# copy
+
+# 参照渡しの場合
+print(list_a)
+list_b = list_a
+list_b[0] = 'asdfg'
+print(list_a)
+
+# copyを使った場合
+list_b = list_a.copy()
+list_b[0] = 'qwert'
+print(list_b)
 print(list_a)
