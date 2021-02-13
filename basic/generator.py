7642a88f2c57275489b22e22d2d4ce5b8b81d363
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-02-13 21:29:29 +0900

    practice pythons generator

--- basic/generator.py
+++ basic/generator.py
@@ -2,10 +2,20 @@
 def generator(max):
     print('ジェネレータ作成')
     for n in range(max):
-        yield n 
-        print('yeild実行')
+        try:
+            x = yield n 
+            print('x = {}'. format(x))
+            print('yeild実行')
+        except ValueError as e:
+            print('throwを実行しました')
 
 gen = generator(10)
+next(gen)
+gen.send(100)
+# 例外をスローする
+#gen.throw(ValueError('Invaild Error'))
+# クローズ
+gen.close()
 
-for x in gen:
-    print('x = {}'. format(x))
+# クローズしているので、ジェネレータ関数は実行されない
+next(gen)
